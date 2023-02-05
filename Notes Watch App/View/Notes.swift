88081d21ce3f1e1d2de37dd945040aaa53c9a1b8
Erge AKOVA <ergeak@gmail.com>
2023-02-06 00:48:02 +0300

    added save, load, and delete note function

--- Notes Watch App/View/Notes.swift
+++ Notes Watch App/View/Notes.swift
@@ -35,17 +35,75 @@ struct Notes: View {
                 .foregroundColor(.accentColor)
             }//MARK: HSTACK
             Spacer()
-            Text("\(notes.count)")
+            
+            if notes.count >= 1 {
+                List {
+                    ForEach(notes) { note in
+                        HStack{
+                            Capsule()
+                                .frame(width: 4)
+                                .foregroundColor(.accentColor)
+                            Text(note.text)
+                                .lineLimit(1)
+                                .padding(.leading, 5)
+                        } //MARK: HSTACK
+                    } //MARK: FOREACH
+                    .onDelete(perform: delete)
+                }//MARK: LIST
+            } else {
+                Spacer()
+                Image(systemName: "note.text")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundColor(.gray)
+                    .opacity(0.25)
+                    .padding(25)
+                Spacer()
+            }
+            
         }//MARK: - VSTACK
         .navigationTitle("Notes")
+        .onAppear {
+            load()
+            dump(notes)
+        }
     }
     
     //MARK: - FUNCTION
     
-    func save(){
-        dump(notes)
+    func getDocumentDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
     }
     
+    func save(){
+        do {
+            let data = try JSONEncoder().encode(notes)
+            let url = getDocumentDirectory().appendingPathComponent("notes")
+            try data.write(to: url)
+        } catch {
+            print("Saving data has failed!")
+        }
+    }
+    
+    func load() {
+        DispatchQueue.main.async {
+            do {
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                let data = try Data(contentsOf: url)
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            } catch {
+                print("loading data has failed!")
+            }
+        }
+    }
+    
+    func delete(offsets: IndexSet) {
+        withAnimation {
+            notes.remove(atOffsets: offsets)
+            save()
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
