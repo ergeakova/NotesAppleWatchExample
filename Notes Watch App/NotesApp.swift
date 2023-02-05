29a1b306b02e7ffc29691c0f1718cc44d6943dcf
Erge AKOVA <ergeak@gmail.com>
2023-02-05 19:45:35 +0300

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by erge on 5.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
